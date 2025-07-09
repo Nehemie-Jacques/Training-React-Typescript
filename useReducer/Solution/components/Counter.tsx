07746a12c0d4dc9f91cae97515a3c27169fc3868
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-07-09 20:05:05 +0100

    useEffect-Error

---
+++ useReducer/Solution/components/Counter.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useReducer } from "react";
+import { CounterReducer, CounterState } from "../reducers/counterReducer"
+
+const initialState: CounterState = { count : 0 };
+
+const Counter = () => { 
+    const [state, dispatch] = useReducer(CounterReducer, initialState);
+
+    const increment = () => {
+        dispatch({ type: "INCREMENT" });
+    }
+
+    const decrement = () => {
+        dispatch({ type: "DECREMENT" });
+    }
+
+    return (
+        <div>
+            <h2>Count: {state.count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter;
